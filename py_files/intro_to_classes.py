d6831cda3db42b002ab9452135c4064c9dee5491
threedee003 <74320520+threedee003@users.noreply.github.com>
2021-10-21 21:57:15 +0530

    Add files via upload

---
+++ py_files/intro_to_classes.py
@@ -0,0 +1,22 @@
+class Item:
+    def calculate_total_price(self, x, y):
+        return x*y
+
+
+
+
+
+item1 = Item()
+item1.name = "Iphone11"
+item1.price = 50000
+item1.quantity = 5
+print(item1.calculate_total_price(item1.price,item1.quantity))
+
+
+item2 = Item()
+item2.name = "Hp pavillion"
+item2.price = 70000
+item2.quantity = 4
+print(item2.calculate_total_price(item2.price,item2.quantity))
+
+
