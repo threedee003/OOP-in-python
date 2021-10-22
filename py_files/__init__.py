c5aea6a20021d0c082eda0e05fec3ad9dc971389
Tribikram Dhar <74320520+threedee003@users.noreply.github.com>
2021-10-22 12:56:01 +0530

    Update __init__.py

--- py_files/__init__.py
+++ py_files/__init__.py
@@ -5,6 +5,7 @@ Date : 21.10.2021
 
 
 class Item:
+    pay_rate = 0.9                                                                                #percentage of price after discount
     def __init__(self, name : str, price : float, quantity : int):
         assert price >= 0 , f"Price {price} is not greater than zero"                              # makes sure if the value is greater than zero
         assert quantity >= 0 , f"Quantity {quantity} is not greater than zero"
@@ -28,4 +29,7 @@ item2 = Item("Hp pavillion",70000,4)
 
 print(item1.calculate_total_price())
 print(item2.calculate_total_price())
+
+print(Item.__dict__)                       # prints all attributes in class level
+print(item1.__dict__)                      # prints all attributes in instance level
 
